30f3590733bae921e15a1cbbe1b124bc7980e2eb
Ju Yejin <linenive@naver.com>
2024-06-15 11:40:43 +0900

    플레이어 방향키 입력 움직임 구현

--- salaryman_simulator/src/main.rs
+++ salaryman_simulator/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::{
+    input::*,
     prelude::*,
     sprite::{MaterialMesh2dBundle, Mesh2dHandle},
 };
@@ -10,12 +11,14 @@ fn main() {
     let mut app = App::new();
     app.add_plugins((DefaultPlugins, HelloPlugin));
     app.register_type::<Name>(); // 인스펙터에 표시하기 위해 Name 타입을 등록
+    app.register_type::<Person>();
 
     #[cfg(feature = "debug")]
     // Debug hierarchy inspector
     app.add_plugins(WorldInspectorPlugin::new());
     // Startup system (cameras)
     app.add_systems(Startup, sprite_setup);
+    app.add_systems(Update, player_movement);
     // Run the app
     app.run();
 }
@@ -30,16 +33,24 @@ fn sprite_setup(
     commands.spawn(Camera2dBundle::default());
 }
 
-#[derive(Component)]
-struct Person;
+#[derive(Reflect, Component)]
+struct Person {
+    speed: f32,
+}
+
+impl Default for Person {
+    fn default() -> Self {
+        Person { speed: 200.0 }
+    }
+}
 
 #[derive(Reflect, Component)]
 struct Name(String);
 
 fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+    commands.spawn((Person::default(), Name("Elaina Proctor".to_string())));
+    commands.spawn((Person::default(), Name("Renzo Hume".to_string())));
+    commands.spawn((Person::default(), Name("Zayna Nieves".to_string())));
 }
 
 #[derive(Component)]
@@ -54,7 +65,7 @@ fn add_player(
     let color = Color::hsl(0.0, 0.95, 0.7);
     commands.spawn((
         Player,
-        Person,
+        Person::default(),
         Name("Player".to_string()),
         MaterialMesh2dBundle {
             mesh: shape,
@@ -87,6 +98,32 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
         if name.0 == "Elaina Proctor" {
             name.0 = "Elaina Hume".to_string();
             break; // We don’t need to change any other names
+        }
+    }
+}
+
+fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&mut Transform, &Person), With<Player>>,
+) {
+    for (mut transform, player) in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+
+        if direction.length_squared() > 0.0 {
+            transform.translation += direction.normalize() * player.speed * 0.02;
         }
     }
 }
